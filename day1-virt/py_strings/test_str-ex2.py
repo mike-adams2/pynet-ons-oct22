6c24324435ca6e5a27832af76a2120f6cc48fb12
mike-adams2 <mja_97@hotmail.com>
2022-10-06 13:54:57 -0500

    test

---
+++ day1-virt/py_strings/test_str-ex2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#a. Create a python script that prompts for an IP address.
+#b. Use #! at top of file; make executable
+#c. split on '.'      
+#d. Print out four octets with column width of 12; left aligned.
+#e. Check your code into git & github
+
+ipaddr = input("IP Address: ")
+octets = ipaddr.split(".")
+
+print()
+print(f"{octets[0]:<10}{octets[1]:<12}{octets[2]:<12}{octets[3]:<12}")
+print()
---
+++ day1-virt/py_strings/test_str_ex1.py
@@ -0,0 +1,14 @@
+name1 = "homer Simpson"
+name2 = "Bart Simpson"
+name3 = "Krusty Clown"
+name4 = "lisa"
+
+print()
+print(f"{name1:<30}")
+print(f"{name2:>30}")
+print(f"{name3:>30}")
+print(f"{name4:>30}")
+print()
+print()
+print()
+print(f"{name1:>30}")
